fd3e8fb9552ef53317537b788dbd3808369fdc97
Irina Y. Wang <iywang@tigercpu.princeton.edu>
2022-04-26 19:01:13 -0400

    updates

--- facility.sh
+++ facility.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=facilitytest
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
-#SBATCH --mem-per-cpu=10
-#SBATCH --time=60:00:00
-#SBATCH -o /scratch/gpfs/iywang/mro_results/facility/m100n20_K500_r10/facility_test_%A.txt
+#SBATCH --cpus-per-task=25
+#SBATCH --mem-per-cpu=5
+#SBATCH --time=40:00:00
+#SBATCH -o /scratch/gpfs/iywang/mro_results/facility/m50n10_K100_r20_1/facility_test_%A.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
 
@@ -16,5 +16,5 @@ module load anaconda3
 #module load gurobi/9.5.1
 conda activate mroenv
 
-python facility/facilityseparate_testing.py 
+python facility/facilityseparate_testing1.py 
 
