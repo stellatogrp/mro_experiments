709e870bcfc152398dc48d4fd5b6a56b1d05a89c
Irina Y. Wang <iywang@tigercpu.princeton.edu>
2022-06-14 15:18:28 -0400

    edit plots

--- facility/plots.py
+++ facility/plots.py
@@ -24,34 +24,31 @@ eps_tot = M
 
 plt.figure(figsize=(10, 6))
 for K_count in np.arange(0,len(K_nums),1):
-    plt.plot(eps_nums, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Opt_val"], linestyle='-', marker = 'o', label="$K = {}$".format(K_nums[K_count]),alpha = 0.6)
-plt.xlabel("$\epsilon^2$")
-plt.xscale("log")
+    plt.plot(eps_nums**0.5, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Opt_val"], linestyle='-', marker = 'o', label="$K = {}$".format(K_nums[K_count]),alpha = 0.6)
+plt.xlabel("$\epsilon$")
 plt.title("In-sample objective value")
 plt.legend(loc = "lower right")
 plt.save("objectives.pdf")
 
 plt.figure(figsize=(10, 6))
 for K_count in np.arange(0,len(K_nums),1):
-    plt.plot(eps_nums, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Eval_val"], label="$K = {}$".format(K_nums[K_count]),linestyle='-', marker='o', alpha=0.5)
-plt.xlabel("$\epsilon^2$")
-plt.xscale("log")
+    plt.plot(eps_nums**0.5, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Eval_val"], label="$K = {}$".format(K_nums[K_count]),linestyle='-', marker='o', alpha=0.5)
+plt.xlabel("$\epsilon$")
 plt.legend(loc = "lower right")
 plt.title(r"$1-\beta$ (probability of constraint satisfaction)(7)")
 plt.savefig("constraint_satisfaction.pdf")
 
 plt.figure(figsize=(10, 6))
 for K_count in np.arange(0,len(K_nums),1):
-    plt.plot(eps_nums, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Eval_val1"], label="$K = {}$".format(K_nums[K_count]),linestyle='-', marker='o', alpha=0.5)
-plt.xlabel("$\epsilon^2$")
-plt.xscale("log")
+    plt.plot(eps_nums**0.5, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Eval_val1"], label="$K = {}$".format(K_nums[K_count]),linestyle='-', marker='o', alpha=0.5)
+plt.xlabel("$\epsilon$")
 plt.legend(loc = "lower right")
 plt.title(r"$1-\beta$ (probability of constraint satisfaction)(8)")
 plt.savefig("constraint_satisfaction_strict.pdf")
 
 plt.figure(figsize=(10, 6))
 for i in np.arange(0,len(eps_nums),3):
-    plt.plot(K_nums, dftemp.sort_values(["Epsilon","K"])[i*len(K_nums):(i+1)*len(K_nums)]["solvetime"], linestyle='-', marker='o', label="$\epsilon = {}$".format(np.round(eps_nums[i], 5)))
+    plt.plot(K_nums, dftemp.sort_values(["Epsilon","K"])[i*len(K_nums):(i+1)*len(K_nums)]["solvetime"], linestyle='-', marker='o', label="$\epsilon = {}$".format(np.round(eps_nums[i]**0.5, 5)))
 plt.xlabel("$K$ (Number of clusters)")
 plt.title("Time (s)")
 plt.yscale("log")
--- portfolio/cont/portcont_testing.py
+++ portfolio/cont/portcont_testing.py
@@ -203,38 +203,3 @@ if __name__ == '__main__':
     np.save(Path("/scratch/gpfs/iywang/mro_results/" +
             foldername + "/x_sols.npy"), x_sols)
     dftemp.to_csv('/scratch/gpfs/iywang/mro_results/' + foldername + '/df.csv')
-
-    plt.rcParams.update({
-        "text.usetex":True,
-        "font.size":18,
-        "font.family": "serif"
-    })
-    
-    plt.figure(figsize=(10, 6))
-    for K_count in np.arange(0,len(K_nums),2):
-        plt.plot(eps_nums, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["Opt_val"], linestyle='-', marker = 'o', label="$K = {}$".format(K_nums[K_count]),alpha = 0.6)
-    plt.xlabel("$\epsilon^2$")
-    plt.xscale("log")
-    plt.title("In-sample objective value")
-    plt.legend(loc = "lower right")
-    plt.savefig("objectives.pdf")
-
-    plt.figure(figsize=(10, 6))
-    for K_count in np.arange(0,len(K_nums),2):
-        plt.plot(eps_nums, dftemp.sort_values(["K","Epsilon"])[K_count*len(eps_nums):(K_count+1)*len(eps_nums)]["satisfy"], label="$K = {}$".format(K_nums[K_count]),linestyle='-', marker='o', alpha=0.5)
-    plt.xlabel("$\epsilon^2$")
-    plt.xscale("log")
-    plt.legend(loc = "lower right")
-    plt.title(r"$1-\beta$ (probability of constraint satisfaction)")
-    plt.savefig("constraint_satisfaction.pdf")
-
-    plt.figure(figsize=(10, 6))
-    for i in np.arange(0,len(eps_nums),3):
-        plt.plot(K_nums, dftemp.sort_values(["Epsilon","K"])[i*len(K_nums):(i+1)*len(K_nums)]["solvetime"], linestyle='-', marker='o', label="$\epsilon = {}$".format(np.round(eps_nums[i], 5)))
-    plt.xlabel("$K$ (Number of clusters)")
-    plt.title("Time (s)")
-    plt.yscale("log")
-    plt.legend(loc = "lower right")
-    plt.savefig("time.pdf")
-
-    print("COMPLETE")
